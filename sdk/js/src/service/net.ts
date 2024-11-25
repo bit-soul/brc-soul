ad85ca43b9d5cb9b66c06dd90346d43807052690
bit-soul <marsOwnershipdao@aliyun.com>
2024-11-25 16:15:54 +0800

    add net.ts

---
+++ sdk/js/src/service/net.ts
@@ -0,0 +1,29 @@
+import { fetchData } from '../utils/utils';
+
+async function getFollower(did: number, size: number, page: number) {
+  const result = {};
+
+  if (!did) {
+    result['code'] = -1;
+    result['mess'] = 'did not valid';
+    return result;
+  }
+
+  const url = global.config.brc_soul_api + `/api/follower?did=${did}`;
+  const json: any = await fetchData(url);
+  return json;
+}
+
+async function getFollowing(did: number, size: number, page: number) {
+  const result = {};
+
+  if (!did) {
+    result['code'] = -1;
+    result['mess'] = 'did not valid';
+    return result;
+  }
+
+  const url = global.config.brc_soul_api + `/api/following?did=${did}`;
+  const json: any = await fetchData(url);
+  return json;
+}
