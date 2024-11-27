a27af93bd6682cd6c02a9110e4acd4970e850358
bit-soul <marsOwnershipdao@aliyun.com>
2024-11-27 15:10:02 +0800

    export all api in index.ts

--- sdk/js/src/index.ts
+++ sdk/js/src/index.ts
@@ -1,10 +1,10 @@
 global.env = 'release';
 global.config = require('./config/release');
 
-import { sortObject, checkSign } from './utils/utils';
-import { getPersonByDid, getPersonByAddr, getDid, getDids, getBatchDids } from './service/did';
-import { getGrp, getGrps, getMyGrps, getBatchGrps, getLeaveSBT } from './service/grp';
-import { getFollower, getFollowing } from './service/net';
-import { getSbt } from './service/sbt';
-import { getVcc, getVccs, getMyVccs, getBatchVccs, getBurnSBT } from './service/vcc';
-import { getMembers, getMyCoidSeq } from './service/misc';
+export { sortObject, checkSign } from './utils/utils';
+export { getPersonByDid, getPersonByAddr, getDid, getDids, getBatchDids } from './service/did';
+export { getGrp, getGrps, getMyGrps, getBatchGrps, getLeaveSBT } from './service/grp';
+export { getFollower, getFollowing } from './service/net';
+export { getSbt } from './service/sbt';
+export { getVcc, getVccs, getMyVccs, getBatchVccs, getBurnSBT } from './service/vcc';
+export { getMembers, getMyCoidSeq } from './service/misc';
