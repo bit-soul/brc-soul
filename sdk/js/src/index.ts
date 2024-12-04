ff7654bf32a82ee09925992ee29f1defc02c432d
bit-soul <marsOwnershipdao@aliyun.com>
2024-12-04 17:54:26 +0800

    publish 0.0.5

--- sdk/js/src/index.ts
+++ sdk/js/src/index.ts
@@ -3,8 +3,8 @@ global.config = require('./config/release');
 
 export { sortObject, checkSign } from './utils/utils';
 export { getPersonByDid, getPersonByAddr, getDid, getDids, getBatchDids } from './service/did';
-export { getGrp, getGrps, getMyGrps, getBatchGrps, getLeaveSBT } from './service/grp';
+export { getGrp, getGrps, getMyGrps, getBatchGrps, getGrpMembers, getLeaveSBT } from './service/grp';
 export { getFollower, getFollowing } from './service/net';
 export { getSbt } from './service/sbt';
-export { getVcc, getVccs, getMyVccs, getBatchVccs, getBurnSBT } from './service/vcc';
-export { getMembers, getMyCoidSeq } from './service/misc';
+export { getVcc, getVccs, getMyVccs, getBatchVccs, getVccMembers, getBurnSBT } from './service/vcc';
+export { getMyCoidSeq } from './service/misc';
