cc14b95f2b4c3e831e866264a7b566d917dcc1d2
bit-soul <marsOwnershipdao@aliyun.com>
2024-12-10 19:27:51 +0800

    add npm run local, it can be used to do some test with real api

--- sdk/js/main.js
+++ sdk/js/main.js
@@ -1,22 +1,13 @@
 const dotenvx = require('@dotenvx/dotenvx');
 switch(process.env.APP_ENV) {
-case 'dev':
-  dotenvx.config({path:'./env/dev.env'});
-  break;
-case 'local':
-  dotenvx.config({path:'./env/local.env'});
-  break;
-case 'pre':
-  dotenvx.config({path:'./env/pre.env'});
-  break;
-case 'prod':
-  dotenvx.config({path:'./env/prod.env'});
+case 'release':
+  dotenvx.config({path:'./env/release.env'});
   break;
 case 'unittest':
   dotenvx.config({path:'./env/unittest.env'});
   break;
 default:
-  dotenvx.config({path:'./env/local.env'});
+  dotenvx.config({path:'./env/unittest.env'});
   break;
 }
 
