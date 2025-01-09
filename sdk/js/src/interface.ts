108c9d06827116dd748ca63803cd87cdcabf3ac0
bit-soul <marsOwnershipdao@aliyun.com>
2025-01-09 11:32:34 +0800

    don't use const enum

--- sdk/js/src/interface.ts
+++ sdk/js/src/interface.ts
@@ -1,14 +1,14 @@
 /**
  * @description User-Service parameters
  */
-export const enum SbtState {
+export enum SbtState {
   NOTMINT = 1,
   MINTED = 2,
   BURNED = 3,
   CANCELED = 4,
 }
 
-export const enum BurnType {
+export enum BurnType {
   BURN_TYPE_COID = 1,
   BURN_TYPE_VCID = 2,
   BURN_TYPE_FLAG = 3,
@@ -16,7 +16,7 @@ export const enum BurnType {
   BURN_TYPE_FLAG_AND_TIME = 5,
 }
 
-export const enum CancelType {
+export enum CancelType {
   CANCEL_TYPE_VCID = 2,
   CANCEL_TYPE_FLAG = 3,
   CANCEL_TYPE_TIME = 4,
