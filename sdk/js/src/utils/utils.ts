eedb00fbf66711e759738369e9f38e4a39c49fe2
bit-soul <marsOwnershipdao@aliyun.com>
2025-01-10 16:05:10 +0800

    fix bug

--- sdk/js/src/utils/utils.ts
+++ sdk/js/src/utils/utils.ts
@@ -1,11 +1,11 @@
-let fetch = window && window.fetch ? window.fetch : null;
+let fetch = typeof window !== 'undefined' && window.fetch ? window.fetch : null;
 let bip322 = null;
 let proxyagent = null;
 const fetch_pack_name = 'node-fetch';
 const bip322_pack_name = 'bip322-js';
 const proxyagent_pack_name = 'socks-proxy-agent';
 const require_to_avoid_pack = require;
-if (typeof window !== 'undefined') {
+if (typeof window === 'undefined') {
   fetch = require_to_avoid_pack(fetch_pack_name);
   bip322 = require_to_avoid_pack(bip322_pack_name);
   proxyagent = require_to_avoid_pack(proxyagent_pack_name);
